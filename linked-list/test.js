7c995c7fc2069f2ca58f9679c94c27ede07eb929
seungheee <lsh3431@gmail.com>
2023-09-28 16:04:04 +0900

    ✨ test linked list

--- linked-list/test.js
+++ linked-list/test.js
@@ -1,1 +1,39 @@
+/**
+ * 연결 리스트 뒤집기
+ * @param sll
+ * @returns {*|null|HTMLHeadElement}
+ */
+function reverseSingleLinkedList(sll){
+    let node = sll.head;
+    let prev = null;
+    while (node){
+        let temp = node.next;
+        node.next = prev;
+        prev = node;
+        if(!temp){
+            break;
+        }
+        node = temp;
+    }
+    return node;
+}
 
+/**
+ * 연결리스트에서 중복된 항목 제거하기 (해시테이블 사용)
+ * @param sll
+ */
+function deleteDuplicateInUnsortedSll(sll){
+    let track = [];
+
+    let temp = sll.head;
+    let prev = null;
+    while (temp){
+        if(track[temp.data]){
+            prev.next = temp.next;
+        }else{
+            track[temp.data] = true;
+            prev = temp;
+        }
+        temp = temp.next;
+    }
+}
