fd3ba2c2d1db86d6c9498565dc48ccd316a6347e
seungheee <lsh3431@gmail.com>
2023-11-16 23:53:09 +0900

    ✨ graph for delete edge, vertex

--- graph/note.js
+++ graph/note.js
@@ -13,3 +13,28 @@ UndirectedGraph.prototype.addEdge = function (vertex1, vertex2, weight){
     this.edges[vertex1][vertex2] = weight;
     this.edges[vertex2][vertex1] = weight;
 }
+
+/**
+ * 간선과 정점 삭제
+ * @param vertex1
+ * @param vertex2
+ */
+UndirectedGraph.prototype.removeEdge = function (vertex1, vertex2){
+    if(this.edges[vertex1] && this.edges[vertex1][vertex2] != undefined){
+        delete this.edges[vertex1][vertex2];
+    }
+    if(this.edges[vertex2] && this.edges[vertex2][vertex1] != undefined){
+        delete this.edges[vertex2][vertex1]
+    }
+}
+
+/**
+ * 전체 정점 삭제
+ * @param vertex
+ */
+UndirectedGraph.prototype.removeVertex = function (vertex){
+    for(let adjacentVertex in this.edges[vertex]){
+        this.removeEdge(adjacentVertex,vertex);
+    }
+    delete this.edges[vertex];
+}
