55fbdf9116b59057101ada840d5eaa8f585e69a0
seungheee <lsh3431@gmail.com>
2023-11-19 23:47:04 +0900

    ✨ Directed Graph

--- graph/note.js
+++ graph/note.js
@@ -38,3 +38,35 @@ UndirectedGraph.prototype.removeVertex = function (vertex){
     }
     delete this.edges[vertex];
 }
+
+/**
+ * 지향성 그래프
+ * @constructor
+ */
+function DirectedGraph(){
+    this.edges = {};
+}
+
+DirectedGraph.prototype.addVertex = function (vertex){
+    this.edges[vertex] = {};
+}
+
+DirectedGraph.prototype.addEdge = function (origVertex, destVertex, weight){
+    if(weight === undefined){
+        weight = 0;
+    }
+    this.edges[origVertex][destVertex] = weight;
+}
+
+DirectedGraph.prototype.removeEdge = function (origVertex, destVertex){
+    if(this.edges[origVertex] && this.edges[origVertex][destVertex] != undefined){
+        delete this.edges[origVertex][destVertex];
+    }
+}
+
+DirectedGraph.prototype.removeVertex = function (vertex){
+    for(let adjacentVertex in this.edges[vertex]){
+        this.removeEdge(adjacentVertex, vertex);
+    }
+    delete this.edges[vertex];
+}
