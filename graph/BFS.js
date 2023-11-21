a79a5cf9ad806a1ea7745dafcbef6edf6a7893e0
seungheee <lsh3431@gmail.com>
2023-11-21 23:50:02 +0900

    ✨ BFS

---
+++ graph/BFS.js
@@ -0,0 +1,19 @@
+/**
+ * 너비 우선 검색 (BFS): 그래프에서 연결된 노드와 해당 노드들 간의 간선을 순서대로 검색하는 알고리즘
+ */
+
+DirectedGraph.prototype.traverseBFS = function (vertex, fn){
+    let queue = [], visited = {};
+    queue.push(vertex);
+
+    while (queue.length){
+        vertex = queue.shift();
+        if(!visited[vertex]){
+            visited[vertex] = true;
+            fn(vertex);
+            for(let adjacentVertex in this.edges[vertex]){
+                queue.push(adjacentVertex);
+            }
+        }
+    }
+}
